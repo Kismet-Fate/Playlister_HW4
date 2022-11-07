ff0e05572c1767234b16ee71da29e3b271ebc02c
Kismet-Fate <yoloswagftw545@gmail.com>
2022-11-07 13:51:24 -0500

    log out works now

--- client/src/components/WorkspaceScreen.js
+++ client/src/components/WorkspaceScreen.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import SongCard from './SongCard.js'
 import MUIEditSongModal from './MUIEditSongModal'
@@ -6,6 +7,7 @@ import MUIRemoveSongModal from './MUIRemoveSongModal'
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import { GlobalStoreContext } from '../store/index.js'
+import AuthContext from '../auth/index.js'
 /*
     This React component lets us edit a loaded list, which only
     happens when we are on the proper route.
@@ -14,8 +16,14 @@ import { GlobalStoreContext } from '../store/index.js'
 */
 function WorkspaceScreen() {
     const { store } = useContext(GlobalStoreContext);
+    const { auth } = useContext(AuthContext);
     store.history = useHistory();
-    
+    useEffect(()=>{
+        if(auth === null || !auth.loggedIn){
+            store.history.push("/");
+        }
+    },//[store.currentList]
+    );
     let modalJSX = "";
     if (store.isEditSongModalOpen()) {
         modalJSX = <MUIEditSongModal />;
@@ -30,6 +38,7 @@ function WorkspaceScreen() {
             sx={{ width: '100%', bgcolor: 'background.paper' }}
         >
             {
+                store.currentList ? 
                 store.currentList.songs.map((song, index) => (
                     <SongCard
                         id={'playlist-song-' + (index)}
@@ -37,7 +46,7 @@ function WorkspaceScreen() {
                         index={index}
                         song={song}
                     />
-                ))  
+                ))  : ""
             }
          </List>            
          { modalJSX }
